3b86e5bd01b3de0b0f5ef73dc7946068e2183c0c
Przemysław Ciżmiński <cizminski@interia.pl>
2026-01-12 19:25:29 +0100

    Created task.

--- src/main/java/core/basesyntax/dao/StorageDaoImpl.java
+++ src/main/java/core/basesyntax/dao/StorageDaoImpl.java
@@ -5,9 +5,39 @@ import core.basesyntax.model.User;
 
 public class StorageDaoImpl implements StorageDao {
     private static Long index = 0L;
+    private static final Integer MIN_AGE = 18;
+    private static final int MIN_CHAR = 6;
 
     @Override
     public User add(User user) {
+        if (user.getLogin() == null) {
+            throw new RegistrationException("Login can't be null");
+        }
+        if (user.getPassword() == null) {
+            throw new RegistrationException("Password can't be null");
+        }
+        if (user.getAge() == null) {
+            throw new RegistrationException("Age can't be null");
+        }
+        if (user.getAge() < MIN_AGE) {
+            throw new RegistrationException("Not valid age: "
+                    + user.getAge() + ". Min allowed age is " + MIN_AGE);
+        }
+        if (user.getPassword().length() < MIN_CHAR) {
+            throw new RegistrationException("To short password: "
+                    + user.getPassword() + ". Min allowed password is " + MIN_CHAR);
+        }
+        if (user.getLogin().length() < MIN_CHAR) {
+            throw new RegistrationException("To short login age: "
+                    + user.getLogin() + ". Min allowed login is " + MIN_CHAR);
+        }
+        for (User existingUser : Storage.people) {
+            if (existingUser.getLogin().equals(user.getLogin())) {
+                throw new RegistrationException("Login already exists: " + user.getLogin());
+            }
+
+        }
+
         user.setId(++index);
         Storage.people.add(user);
         return user;
--- src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
+++ src/test/java/core/basesyntax/dao/StorageDaoImplTest.java
@@ -1,32 +1,53 @@
 package core.basesyntax.dao;
 
 import core.basesyntax.model.User;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StorageDaoImplTest {
     private StorageDaoImpl storageDao;
+
     @BeforeEach
     void beforeEach() {
         storageDao = new StorageDaoImpl();
     }
+
     @Test
     void nullLoginException() {
-        User user = new User(null, "abcdefg", 25);
-        assertThrows(RegistrationException.class, () -> storageDao.add(user));
-    }
-    @Test
-    void nullPasswordException() {
-        User user = new User("abcdefg", null, 25);
-        assertThrows(RegistrationException.class, () -> storageDao.add(user));
-    }
-    @Test
-    void nullAgeException() {
-        User user = new User("abcdefg", "abcdefg", null);
-        assertThrows(RegistrationException.class, () -> storageDao.add(user));
+        User actual = new User(null, "abcdef", 25);
+        assertThrows(RegistrationException.class, () -> storageDao.add(actual));
     }
 
+    @Test
+    void nullPasswordException() {
+        User actual = new User("abcdef", null, 25);
+        assertThrows(RegistrationException.class, () -> storageDao.add(actual));
+    }
+
+    @Test
+    void nullAgeException() {
+        User actual = new User("abcdef", "abcdef", null);
+        assertThrows(RegistrationException.class, () -> storageDao.add(actual));
+    }
+
+    @Test
+    void toSmallLoginException() {
+        User actual = new User("abc", "abcdef", 25);
+        assertThrows(RegistrationException.class, () -> storageDao.add(actual));
+    }
+
+    @Test
+    void toSmallPasswordException() {
+        User actual = new User("abcdef", "abc", 25);
+        assertThrows(RegistrationException.class, () -> storageDao.add(actual));
+    }
+
+    @Test
+    void duplicateLoginException() {
+        User actual = new User("abcdef", "abcdef", 25);
+        storageDao.add(actual);
+        assertThrows(RegistrationException.class, () -> storageDao.add(actual));
+    }
 }
